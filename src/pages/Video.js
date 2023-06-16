485a98e7ee65fb70c4ab177d6204cd4291d43152
Igor Dias <igorverissimo02@hotmail.com>
2023-06-15 21:46:12 -0300

    Código da aula 3. Adicionado o footer da aplicação

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import "./video.css";
+import VideoFooter from "./components/footer/VideoFooter";
 function Video() {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
@@ -22,6 +23,7 @@ function Video() {
         onClick={handdleStart}
         src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
       ></video>
+      <VideoFooter/>
     </div>
   );
 }
