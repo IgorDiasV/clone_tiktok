bff7efdc8d853dcfe83a5db7d16ef668fe13bfee
Igor Dias <igorverissimo02@hotmail.com>
2023-06-17 10:58:27 -0300

    código inicial da aula 4. Implementado a parte visual da sidebar

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./video.css";
 import VideoFooter from "./components/footer/VideoFooter";
+import VideoSideBar from "./components/videoSideBar/VideoSideBar";
 function Video() {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
@@ -23,6 +24,7 @@ function Video() {
         onClick={handdleStart}
         src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
       ></video>
+      <VideoSideBar/>
       <VideoFooter/>
     </div>
   );
